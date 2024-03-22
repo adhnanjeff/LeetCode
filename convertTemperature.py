14c41391c37e6767dc130fb0cb87cc250d8212c2
Adhnan Jeff M S <145822540+adhnanjeff@users.noreply.github.com>
2024-03-22 23:42:40 +0530

    Delete convertTemperature.py

--- convertTemperature.py
+++
@@ -1,6 +0,0 @@
-#Problem 2469
-class Solution(object):
-    def convertTemperature(self, celsius):
-        kelvin = 273.15 + celsius
-        fahrenheit = (celsius * 9/5 + 32)
-        return [kelvin, fahrenheit]
