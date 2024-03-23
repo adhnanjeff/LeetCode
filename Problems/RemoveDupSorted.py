d6c8438caa73f9ccf1f03af138b06a7febebbc9a
Adhnan Jeff M S <145822540+adhnanjeff@users.noreply.github.com>
2024-03-24 00:16:26 +0530

    Delete Problems directory

--- Problems/RemoveDupSorted.py
+++
@@ -1,4 +0,0 @@
-class Solution(object):
-    def removeDuplicates(self, nums):
-        nums[:] = sorted(set(nums))
-        return len(nums)
--- Problems/convertTemperature.py
+++
@@ -1,5 +0,0 @@
-class Solution(object):
-    def convertTemperature(self, celsius):
-        kelvin = 273.15 + celsius
-        fahrenheit = (celsius * 9/5 + 32)
-        return [kelvin, fahrenheit]
