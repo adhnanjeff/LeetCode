c002ceabb8d1ae7ce080daebd06655327194f9bb
Adhnan Jeff M S <145822540+adhnanjeff@users.noreply.github.com>
2024-03-29 10:41:02 +0530

    Add files via upload

---
+++ Problems/2958.LengthOfLongestSubarray.py
@@ -0,0 +1,17 @@
+def maxSubarrayLength(nums, k):
+    n = len(nums)
+    i = j = result = 0
+    dict_data = {}
+
+    while j < n:
+        dict_data[nums[j]] = dict_data.get(nums[j],0) + 1 
+        while i < j and dict_data[nums[j]] > k:
+            dict_data[nums[i]] -= 1  
+            i += 1
+        result = max(result, j - i + 1)
+        j += 1
+    return result
+
+nums = [1, 2, 3, 1, 2, 3, 1, 2]
+ans = maxSubarrayLength(nums, 2)
+print(ans)
