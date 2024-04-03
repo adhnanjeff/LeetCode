6794de3f2880e46116e5b8bc57bb316f1eb5e56f
Adhnan Jeff M S <145822540+adhnanjeff@users.noreply.github.com>
2024-04-04 00:08:50 +0530

    Add files via upload

---
+++ Problems/2879.Display3Rows.py
@@ -0,0 +1,18 @@
+# Import pandas package 
+import pandas as pd 
+	
+# Define a dictionary containing employee data 
+data = {'Name':['Jai', 'Princi', 'Gaurav', 'Anuj'], 
+		'Age':[27, 24, 22, 32], 
+		'Address':['Delhi', 'Kanpur', 'Allahabad', 'Kannauj'], 
+		'Qualification':['Msc', 'MA', 'MCA', 'Phd']} 
+	
+# Convert the dictionary into DataFrame 
+df = pd.DataFrame(data) 
+	
+# select top three rows 
+print(df.loc[1:3]) 
+print(df.head(3)) 
+# select bottom three rows
+
+print(df.tail(3)) 
