64b1a8948e98a139c361427b91314a2ff5bbf757
Adhnan Jeff M S <145822540+adhnanjeff@users.noreply.github.com>
2024-04-11 00:06:06 +0530

    Add files via upload

---
+++ Problems/950.RevealCards.py
@@ -0,0 +1,19 @@
+#Problem 950
+
+from collections import deque
+def deckRevealedIncreasing(deck):
+    deck_sorted = sorted(deck, reverse=True)
+    ordered_deck = deque()
+
+    for card in deck_sorted:
+
+        if len(ordered_deck) > 1:
+            ordered_deck.rotate(1)
+        ordered_deck.appendleft(card)
+
+    return list(ordered_deck) 
+
+deck = [17,13,11,2,3,5,7]
+ans = deckRevealedIncreasing(deck)
+print(ans)
+
