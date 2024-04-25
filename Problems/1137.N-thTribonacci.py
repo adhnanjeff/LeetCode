087739d17f911f7a5f437ae9b3990ac42d8ac302
Adhnan Jeff M S <145822540+adhnanjeff@users.noreply.github.com>
2024-04-25 21:56:04 +0530

    Create 1137.N-thTribonacci.py

---
+++ Problems/1137.N-thTribonacci.py
@@ -0,0 +1,21 @@
+#Problem 1137
+
+class Solution:
+    def tribonacci(self, n):
+        if n == 0:
+            return 0
+        if n == 1 or n == 2:
+            return 1
+        
+        T = [0, 1, 1]
+
+        for i in range(3, n+1):
+            T.append(T[i-1] + T[i-2] + T[i-3])
+
+
+        return T[n]
+    
+s = Solution()
+n = 10
+ans = s.tribonacci(n)
+print(ans)
