3d181314f901fc20a0ff22c3e401083ba6300a9a
Adhnan Jeff M S <145822540+adhnanjeff@users.noreply.github.com>
2024-05-06 20:25:24 +0530

    Add files via upload

---
+++ Daily Challenges/May 2024/2487.RemoveNodes.py
@@ -0,0 +1,20 @@
+#Problem 2487
+#Solved on 6.5.24
+
+class Solution:
+    def removeNodes(self, head):
+        cur = head
+        stack = []
+        while cur:
+            while stack and stack[-1].val < cur.val:
+                stack.pop()
+            stack.append(cur)
+            cur = cur.next
+        
+        nxt = None
+        while stack:
+            cur = stack.pop()
+            cur.next = nxt
+            nxt = cur
+        
+        return cur
