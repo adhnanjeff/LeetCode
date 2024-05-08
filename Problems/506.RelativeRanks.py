6e0e5ff04356dc0d0e3d0916e9c5ae16abfd6544
Adhnan Jeff M S <145822540+adhnanjeff@users.noreply.github.com>
2024-05-08 19:40:18 +0530

    Add files via upload

---
+++ Problems/506.RelativeRanks.py
@@ -0,0 +1,9 @@
+#Problem 506
+#Solved on 8.5.24
+
+class Solution:
+    def findRelativeRanks(self, score):
+        sorted_score = sorted(score, reverse=True)
+        medals = ["Gold Medal", "Silver Medal", "Bronze Medal"]
+        rank_mapping = {score: medals[i] if i < 3 else str(i + 1) for i, score in enumerate(sorted_score)}
+        return [rank_mapping[score] for score in score]
