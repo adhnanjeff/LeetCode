32cf8c4bd6d3ac1ab72daf481201f87696a708fc
Adhnan Jeff M S <145822540+adhnanjeff@users.noreply.github.com>
2024-05-24 13:07:49 +0530

    Add files via upload

---
+++ Problems/1255.MaxScoreWords.py
@@ -0,0 +1,40 @@
+#Problem 1255
+#Solved on 24.5.24
+
+class Solution:
+    def maxScoreWords(self, words: List[str], letters: List[str], score: List[int]) -> int:
+        count = [0] * 26
+        letters_count = [0] * 26
+        
+        for c in letters:
+            count[ord(c) - ord('a')] += 1
+        
+        self.ans = 0
+        self.backtracking(words, score, count, letters_count, 0, 0)
+        return self.ans
+    
+    def backtracking(self, words, score, count, letters_count, pos, temp):
+        for i in range(26):
+            if letters_count[i] > count[i]:
+                return
+        
+        self.ans = max(self.ans, temp)
+        
+        for i in range(pos, len(words)):
+            for c in words[i]:
+                letters_count[ord(c) - ord('a')] += 1
+                temp += score[ord(c) - ord('a')]
+            
+            self.backtracking(words, score, count, letters_count, i + 1, temp)
+            
+            for c in words[i]:
+                letters_count[ord(c) - ord('a')] -= 1
+                temp -= score[ord(c) - ord('a')]
+
+s = Solution()
+words = ["dog","cat","dad","good"] 
+letters = ["a","a","c","d","d","d","g","o","o"] 
+score = [1,0,9,5,0,0,3,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0]
+
+ans = s.maxScoreWords(words, letters, score)
+print(ans)
