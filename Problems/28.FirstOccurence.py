ecf4cddcece57fff9c3a27bd0ae0a09b97277516
Adhnan Jeff M S <145822540+adhnanjeff@users.noreply.github.com>
2024-05-25 20:42:49 +0530

    Add files via upload

---
+++ Problems/28.FirstOccurence.py
@@ -0,0 +1,15 @@
+#Problem 28
+
+class Solution:
+    def strStr(self, haystack, needle):
+        for i in range(len(haystack) - len(needle) + 1):
+            if haystack[i:i+len(needle)] == needle:
+                return i
+        return -1
+    
+
+s = Solution()
+haystack = "hello"
+needle = "ll"
+ans = s.strStr(haystack, needle)
+print(ans)
