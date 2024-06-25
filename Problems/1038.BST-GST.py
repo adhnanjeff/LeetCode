f374f830b85892de803a89be05784fa3c8cd6bd0
Adhnan Jeff M S <145822540+adhnanjeff@users.noreply.github.com>
2024-06-25 22:28:39 +0530

    Add files via upload

---
+++ Problems/1038.BST-GST.py
@@ -0,0 +1,14 @@
+#Problem 1038
+#Solved on 25.6.24
+
+class Solution(object):
+    def __init__(self):
+        self.sum = 0
+    def bstToGst(self, root):
+        if root:
+            self.bstToGst(root.right)  # Traverse the right subtree
+            self.sum += root.val  # Update the sum
+            root.val = self.sum  # Update the current node's value
+            self.bstToGst(root.left)  # Traverse the left subtree
+        return root
+        
