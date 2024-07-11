6700a82a9c2c2a83e7dbbcf68a7bce82c3c6a5e5
Adhnan Jeff M S <145822540+adhnanjeff@users.noreply.github.com>
2024-07-11 23:10:04 +0530

    Add files via upload

---
+++ Problems/1190.ReverseSubString.py
@@ -0,0 +1,24 @@
+#Problem 1190
+#Solved on 11.7.24
+
+from collections import deque
+
+class Solution:
+    def reverseParentheses(self, s):
+        ind_stack = deque()
+        res = []
+
+        for char in s:
+            if char == "(":
+                ind_stack.append(len(res)) 
+            elif char == ")": 
+                start_ind = ind_stack.pop()
+                res[start_ind:] = res[start_ind:][::-1]
+            else:
+                res.append(char)
+
+        return "".join(res)
+    
+
+s = Solution()
+print(s.reverseParentheses("a(bcdefghijkl(mno)p)q"))
