43dc17cc1ba83184d7a69cd3df3d5ddadb303d5f
Adhnan Jeff M S <145822540+adhnanjeff@users.noreply.github.com>
2024-09-04 23:34:58 +0530

    Add files via upload

---
+++ Problems/1945.SumOfString.py
@@ -0,0 +1,17 @@
+#Problem 1945
+#Solved on 3.9.24
+
+class Solution(object):
+    def getLucky(self, s, k):
+        number = ''
+        for x in s:
+            number += str(ord(x) - ord('a') + 1)
+        
+        while k > 0:
+            temp = 0
+            for x in number:
+                temp += int(x) 
+            number = str(temp)  
+            k -= 1
+
+        return int(number)
